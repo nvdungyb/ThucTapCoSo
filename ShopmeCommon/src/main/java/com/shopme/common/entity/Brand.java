5790d48aa7a19078062d9b2e3c2e22bf01e2ff3d
NvDzung <dung3032003md@gmail.com>
2024-05-22 09:13:41 +0700

    Brand entity

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -0,0 +1,28 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 128)
+    private String logo;
+
+    @ManyToMany
+    @JoinTable(name = "brands_categories",
+            joinColumns = @JoinColumn(name = "brand_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+}
