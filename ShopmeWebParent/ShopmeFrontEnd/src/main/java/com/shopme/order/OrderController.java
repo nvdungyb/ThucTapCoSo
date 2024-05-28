6233894ca7db15ca796966121714de0c251d3983
NvDzung <dung3032003md@gmail.com>
2024-05-28 15:00:01 +0700

    update font-end

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderController.java
@@ -0,0 +1,2 @@
+package com.shopme.order;public class OrderController {
+}
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderService.java
@@ -0,0 +1,15 @@
+package com.shopme.order;
+
+import com.shopme.common.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
