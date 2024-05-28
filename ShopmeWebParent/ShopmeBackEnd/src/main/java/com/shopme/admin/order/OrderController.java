151cab713d50ff9d9f7af084d33322b458968dbe
NvDzung <dung3032003md@gmail.com>
2024-05-28 16:59:31 +0700

    Order page, search and enabled order

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderController.java
@@ -1,2 +1,53 @@
-package com.shopme.admin.order;public class OrderController {
+package com.shopme.admin.order;
+
+import com.shopme.admin.customer.CustomerService;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/orders")
+    public String viewFirstPageOrder(Model model) {
+        return viewByPageOrder(1, null, model);
+    }
+
+    @GetMapping("/orders/page/{pageNumber}")
+    public String viewByPageOrder(@PathVariable(name = "pageNumber") int pageNum, @RequestParam(name = "keyword", required = false) String keyword, Model model) {
+        Page<Order> page = orderService.listByPage(pageNum, keyword);
+        List<Order> listOrders = page.getContent();
+
+        long startCount = (pageNum - 1) * CustomerService.CUSTOMER_PER_PAGE + 1;
+        long endCount = startCount + CustomerService.CUSTOMER_PER_PAGE - 1;
+        if (endCount > page.getTotalElements()) {
+            endCount = page.getTotalElements();
+        }
+
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("listOrders", listOrders);
+
+        return "orders/order";
+    }
+
+    @GetMapping("/orders/{id}/enabled/{status}")
+    public String editCustomerEnabledStatus(@PathVariable("id") Integer id, @PathVariable("status") boolean enabled) {
+        orderService.updateCustomerEnabledStatus(id, enabled);
+        return "redirect:/orders";
+    }
+
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderService.java
@@ -1,2 +1,31 @@
-package com.shopme.admin.order;public class OrderService {
+package com.shopme.admin.order;
+
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    private final int ORDER_PER_PAGE = 10;
+
+    public Page<Order> listByPage(int pageNum, String keyword) {
+        Pageable pageable = PageRequest.of(pageNum - 1, ORDER_PER_PAGE);
+        if (keyword != null) {
+            return orderRepository.findAll(keyword, pageable);
+        }
+        return orderRepository.findAll(pageable);
+    }
+
+    public void updateCustomerEnabledStatus(Integer id, boolean enabled) {
+        orderRepository.updateEnabledStatus(id, enabled);
+    }
 }
