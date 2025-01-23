08e4e78516835cb080e2bdffee6b1fa44f2742a3
NvDzung <dung3032003md@gmail.com>
2025-01-24 00:50:59 +0700

    feat: add API for email verification and token validation

--- ShopmeWebParent/ShopmeCustomer/src/main/java/com/shopme/customer/CustomerController.java
+++ ShopmeWebParent/ShopmeCustomer/src/main/java/com/shopme/customer/CustomerController.java
@@ -1,12 +1,16 @@
 package com.shopme.customer;
 
 import com.shopme.advice.exception.EmailAlreadyExistsException;
+import com.shopme.advice.exception.InvalidTokenException;
 import com.shopme.advice.exception.RoleNotFoundException;
+import com.shopme.advice.exception.TooManyRequestsException;
 import com.shopme.common.entity.Customer;
+import com.shopme.mail.MailService;
 import com.shopme.message.dto.request.CustomerRegisterDto;
 import com.shopme.message.ApiResponse;
 import com.shopme.message.dto.request.EmailCheckDto;
 import com.shopme.message.dto.response.CustomerResponseDto;
+import com.shopme.verification.RegisterVerification;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +25,19 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Map;
 
 @RestController
 public class CustomerController {
-    @Autowired
-    private CustomerService customerService;
+    private final CustomerService customerService;
+    private final RegisterVerification registerVerification;
 
     private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    public CustomerController(CustomerService customerService, RegisterVerification registerVerification) {
+        this.customerService = customerService;
+        this.registerVerification = registerVerification;
+    }
 
     @PostMapping("/customer/save")
     public @ResponseBody Customer saveCustomer(@RequestBody Customer customer, RedirectAttributes redirectAttributes) throws IOException {
@@ -52,6 +62,38 @@ public class CustomerController {
                 .path("/customers/register")
                 .build()
         );
+    }
+
+    @PostMapping("/customers/email/verification")
+    public ResponseEntity<?> verificationEmail(@RequestBody Map<String, String> requestBody) throws TooManyRequestsException {
+        String email = requestBody.get("email");
+        logger.info("Email: {}", email);
+        boolean isValidEmail = MailService.isValidEmail(email);
+        if (isValidEmail) {
+            registerVerification.sendVerificationToken(email);
+        }
+
+        return ResponseEntity.status(isValidEmail ? HttpStatus.OK : HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.builder()
+                        .timestamp(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                        .status(isValidEmail ? HttpStatus.OK.value() : HttpStatus.BAD_REQUEST.value())
+                        .message(isValidEmail ? "Verification email has been sent to " + email : "Invalid email address")
+                        .data(null)
+                        .path("/customers/email/verification")
+                        .build());
+    }
+
+    @GetMapping("/customers/email/verify")
+    public ResponseEntity<?> verifyEmail(@RequestParam String token) throws InvalidTokenException {
+        registerVerification.verifyToken(token);
+
+        return ResponseEntity.ok(ApiResponse.builder()
+                .timestamp(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .status(HttpStatus.OK.value())
+                .message("Email has been verified successfully")
+                .data(null)
+                .path("/customers/email/verify")
+                .build());
     }
 
     @PostMapping("/customers/email/uniqueness")
